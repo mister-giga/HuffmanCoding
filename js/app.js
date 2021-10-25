439888e31b8768f5032b1be7410c37928abf1a11
mister-giga <mister-giga@users.noreply.github.com>
2021-10-25 19:40:20 +0000

    deploy: 7af71265b98fe240ec63478b421e6c9159811a82

---
+++ js/app.js
@@ -0,0 +1,54 @@
+﻿var graphNetwork = null;
+var graphData = null;
+
+function destroy() {
+    if (graphNetwork !== null) {
+        graphNetwork.destroy();
+        graphNetwork = null;
+    }
+}
+
+
+window.loadSvgGraph = (data) => {
+    graphData = data;
+    draw();
+}
+
+window.addEventListener("resize", draw);
+
+
+
+function draw() {
+    destroy();
+    var container = document.getElementById("graphviz_svg_div");
+
+    if (!graphData || !container) {
+        return;
+    }
+    
+    var options = {
+        layout: {
+            hierarchical: {
+                direction: 'UD',
+            },
+        },
+        edges: {
+            arrows: "to",
+            smooth: {
+                type: "cubicBezier",
+                forceDirection: "vertical",
+                roundness: 0.4,
+            },
+        },
+        physics: false,
+        interaction: {
+            dragNodes: false,
+        },
+        autoResize: false
+    };
+
+    const jData = JSON.parse(graphData);
+    console.log(jData);
+    graphNetwork = new vis.Network(container, jData, options);
+    console.log(graphNetwork);
+}
